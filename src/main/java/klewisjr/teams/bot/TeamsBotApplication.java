f04036dbf0fa100ae2f0fbe7637d195f4052334e
kdiggety <wakes-pillory.01@icloud.com>
2025-05-27 00:15:49 -0700

    initial working build

---
+++ src/main/java/klewisjr/teams/bot/TeamsBotApplication.java
@@ -0,0 +1,12 @@
+package klewisjr.teams.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsBotApplication.class, args);
+    }
+}
